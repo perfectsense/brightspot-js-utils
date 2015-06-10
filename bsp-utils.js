0928cb5796c3669348c6fbbaa1d72fa8003ccba7
John Pencola <paulpencola@gmail.com>
2015-06-10 11:04:56 -0400

    Adds .editorconfig, Adds debounce function, Updates readme

--- bsp-utils.js
+++ bsp-utils.js
@@ -49,6 +49,29 @@
         };
     })();
 
+    /** Debounces (postpones) execution of a function.
+    *   Returns a function, that, as long as it continues to be invoked, will not
+    *   be triggered. The function will be called after it stops being called for
+    *   N milliseconds. If `immediate` is passed, trigger the function on the
+    *   leading edge, instead of the trailing.
+    */
+    (function() {
+        bsp_utils.debounce = function(wait, debouncedFunction, immediate) {
+            var timeout;
+            return function() {
+                var context = this, args = arguments;
+                var later = function() {
+                    timeout = null;
+                    if (!immediate) debouncedFunction.apply(context, args);
+                };
+                var callNow = immediate && !timeout;
+                clearTimeout(timeout);
+                timeout = setTimeout(later, wait);
+                if (callNow) debouncedFunction.apply(context, args);
+            };
+        }
+    })();
+
     // Detects inserts into the DOM.
     (function() {
         var domInserts = [ ];
